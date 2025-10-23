10ebd53b3068f57b3fedd498e88c15a71714f065
mara5555 <mara.bandila5@gmail.com>
2025-10-23 21:00:37 +0200

    modified:   assignments/a5/inhabits.php 	modified:   assignments/a5/item.php 	modified:   assignments/a5/replika.php 	modified:   assignments/a5/replika_check.php 	modified:   assignments/a5/report.php 	modified:   assignments/a5/task_check.php

--- assignments/a5/replika_check.php
+++ assignments/a5/replika_check.php
@@ -1,9 +1,60 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Replika Check</title>
+    <link href="style.css" rel="stylesheet" type="text/css" media="all">
+  </head>
+  <body>
+    <div class="form-check">
+    <p>You have added: </p>
+      
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+          die("<p>Error: No form data submitted. Please use the form to add a Gestalt.</p>");
+      }
       $config = require __DIR__ . '/../project/bootstrap.php';
  
       $servername = $config['DB_HOST'];
       $username = $config['DB_USER']; 
       $password = $config['DB_PASSWORD'];
       $dbname = $config['DB_NAME'];
-$conn = new mysqli($servername, $username, $password, $dbname);
-?>
+
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      // Check connection
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      }
+    
+      $ReplikaArr = array("ARAR", "EULR", "KLBR", "LSTR", "MNHR", "STAR", "STCR", "SAPR", "KNCR", "ADLR", "FKLR");
+      if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $model = $_POST['model'];
+        $name = $ReplikaArr[model - 1] . "S-23" . $_POST['code'];
+        $arrival_date = $_POST['arrival_date'];
+        $current_role = $_POST['current_role'];
+        $clearance = $_POST['clearance'];
+        $nickname = $_POST['nname'];
+        }
+        
+      $stmt = $conn->prepare("INSERT INTO Replika (RID, Clearance, Legal_Name, Inhabits, Arrival_Date, Assigned, Reports_To, Current_Role, M_ID, Nickname)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+      $rid = NULL;
+      $inhabits = NULL;
+      $assigned = NULL;
+      $reports_to = NULL;
+
+$stmt->bind_param("iisisiisis", $rid, $clearance, $name, $inhabits, $arrival_date, $assigned, $reports_to, $current_role, $model, $nickname);
+      $stmt->bind_param(iisisiisis, $rid, $clearance, $name, $inhabits, $arrival_date, $assigned, $reports_to, $current_role, $model, $nickname);
+      //nuh-uh, no sql injections
+        if ($stmt->execute() === TRUE) {
+        echo "New record created successfully";
+        } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+      ?>
+    </div>
+  </body>
+</html>
