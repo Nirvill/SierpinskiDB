b3fa24d3ee1b7798c2a210c417ec99d04b99d0aa
nirvill <rzherebilovg@gmail.com>
2025-10-23 16:53:35 +0200

    trying to mergt stuff

--- assignments/a5/replika.php
+++ assignments/a5/replika.php
@@ -6,27 +6,29 @@
     <link href="style.css" rel="stylesheet" type="text/css" media="all">
   </head>
   <body>
+    <div class="imprint">
     <h1>Insert a Replika<h1>
     <form action="/replika_check.php">
-      
+     
     <?php
       require_once __DIR__ . '../project/bootstrap.php';
-
+ 
       $servername = $config['DB_HOST'];
       $username = $config['DB_USER'];
       $password = $config['DB_PASSWORD'];
       $dbname = $config['DB_NAME'];
-
+ 
+ 
       // Create connection
       $conn = new mysqli($servername, $username, $password, $dbname);
       // Check connection
       if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
       }
-    
+   
       $sql = "SELECT Model_Code FROM Models";
       $result = $conn->query($sql);
-
+ 
       if ($result->num_rows > 0) {
           // output data of each row
           while($row = $result->fetch_assoc()) {
@@ -36,8 +38,9 @@
       } else {
           echo "0 results";
       }
-
+ 
       $conn->close();
       ?>
+    </div>
   </body>
 </html>
