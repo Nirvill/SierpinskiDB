0c48a15c6a027e9c941ed02d4138c68c489847c8
mara5555 <mara.bandila5@gmail.com>
2025-10-30 16:16:01 +0100

    modified:   assignments/as6/search.html 	modified:   assignments/as6/search.php

--- assignments/as6/search.php
+++ assignments/as6/search.php
@@ -18,20 +18,21 @@ $config = require __DIR__ . '/../project/bootstrap.php';
 if (isset($_POST['search'])) {
     $search = $conn->real_escape_string($_POST['search']);
     $sql = $search ?
-        "SELECT Location_Name
+        "SELECT Location_Name, LID
         FROM Location1
         WHERE Location_Name LIKE '%$search%'" :
 
-        "SELECT Location_Name
+        "SELECT Location_Name, LID
         FROM Location1";
     $result = $conn->query($sql);
  
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo "<tr><td><a href=\"search_result.html\">" . $row['Location_Name'] . "</a></td></tr>";
+            echo "<input type=\"radio\" id=\"" . $row['LID'] . "\" name=\"location\" value=\"". $row['LID'] . "\">";
+            echo "<label for=\"". $row['LID'] ."\">" . $row['Location_Name'] ."</label><br>"
         }
     } else {
-        echo "<tr><td colspan='1'>No results found.</td></tr>";
+        echo "<p>No results found.</p>";
     }
 }
  $conn->close();
