2862b3f08de7bb54791d52d37dff5966673234c2
mara5555 <mara.bandila5@gmail.com>
2025-10-30 16:28:41 +0100

    modified:   assignments/as6/search.html 	modified:   assignments/as6/search.php

--- assignments/as6/search.php
+++ assignments/as6/search.php
@@ -15,25 +15,23 @@ $config = require __DIR__ . '/../project/bootstrap.php';
       if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
       }
-      echo "<p>balls</p>";
 if (isset($_POST['search'])) {
     $search = $conn->real_escape_string($_POST['search']);
     $sql = $search ?
-        "SELECT Location_Name, LID
+        "SELECT Location_Name
         FROM Location1
         WHERE Location_Name LIKE '%$search%'" :
 
-        "SELECT Location_Name, LID
+        "SELECT Location_Name
         FROM Location1";
     $result = $conn->query($sql);
  
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo "<input type=\"radio\" id=\"" . $row['LID'] . "\" name=\"location\" value=\"" . $row['LID'] . "\">";
-            echo "<label for=\"" . $row['LID'] . "\">" . $row['Location_Name'] . "</label><br>";
+            echo "<tr><td><a href=\"search_result.html\">" . $row['Location_Name'] . "</a></td></tr>";
         }
     } else {
-        echo "<p>No results found.</p>";
+        echo "<tr><td colspan='1'>No results found.</td></tr>";
     }
 }
  $conn->close();
