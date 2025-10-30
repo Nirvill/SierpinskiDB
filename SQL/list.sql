c6844b43bc5e573b147415c5553fcc99ee154afe
nirvill <rzherebilovg@gmail.com>
2025-10-30 17:26:43 +0100

    modified:   SQL/list.sql

--- SQL/list.sql
+++ SQL/list.sql
@@ -1,13 +1,3 @@
 USE db_rzherebilov;
-SELECT R.Legal_Name AS Name, 'Replika' AS Type, L.Location_Name AS Location
-        FROM Replika R
-        INNER JOIN Location1 L ON R.Inhabits = L.LID
-        WHERE L.Location_Name LIKE '%STAR%'
- 
-        UNION ALL
- 
-        SELECT G.Legal_Name AS Name, 'Gestalt' AS Type, L.Location_Name As Location
-        FROM Gestalt G
-        INNER JOIN Location1 L ON G.Inhabits = L.LID
-        WHERE L.Location_Name LIKE '%STAR%'
-        ORDER BY Name;
+ALTER TABLE TASK1
+
