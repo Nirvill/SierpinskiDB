a36d5627911f4cabec1aaa8ed49e6772140d96be
mara5555 <mara.bandila5@gmail.com>
2025-11-27 14:57:36 +0100

    modified:   public_html/map.php

--- public_html/map.php
+++ public_html/map.php
@@ -20,7 +20,7 @@
   </body>
 
   <script>
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([0, 0], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
