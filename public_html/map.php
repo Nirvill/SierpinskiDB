fc6639e192d5fb1acf5dc194dc24405a4728519a
mara5555 <mara.bandila5@gmail.com>
2025-11-27 15:44:45 +0100

    modified:   public_html/map.php

--- public_html/map.php
+++ public_html/map.php
@@ -29,7 +29,7 @@
     console.log("Longitude:", lon);
     var map = L.map('map').setView([lat, lon], 13);
     var marker = L.marker([lat, lon]).addTo(map);
-    marker.bindPopup("Your IP address is " + ip).openPopup();
+    marker.bindPopup("Your IP address is <b>" + ip + "</b>").openPopup();
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
