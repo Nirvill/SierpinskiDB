e47e9c1a31369fe034d943bc6376034c5541338d
nirvill <rzherebilovg@gmail.com>
2025-11-27 15:18:43 +0100

    modified:   public_html/map.php

--- public_html/map.php
+++ public_html/map.php
@@ -20,23 +20,23 @@
   <script>
 
 
-    var Latitude = 0;
-    var Longitude = 0;
-    var usrip = {};
-    $.ajax({
-        url: "https://ipapi.co/json/",
-        async: false,
-        dataType: 'json',
-        success: function(data) {
-            Latitude = data.latitude;
-            Longitude = data.longitude;
-            usrip = data.ip;
+    // var Latitude = 0;
+    // var Longitude = 0;
+    // var usrip = {};
+    // $.ajax({
+    //     url: "https://ipapi.co/json/",
+    //     async: false,
+    //     dataType: 'json',
+    //     success: function(data) {
+    //         Latitude = data.latitude;
+    //         Longitude = data.longitude;
+    //         usrip = data.ip;
 
-        }
-    });
+    //     }
+    // });
 
 
-    var map = L.map('map').setView([Latitude, Longitude], 13);
+    var map = L.map('map').setView([0, 0], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
